6a7ff00711ba01f9ebe165e2135bcfa7d8cb7c71
Kevin <kevin.shaughnessy18@gmail.com>
2021-02-21 22:18:15 -0500

    Created Accordion component

---
+++ src/components/Accordion.js
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+
+const Accordion = ({ items }) => {
+  const renderedItems = items.map((item) => {
+    return (
+      <div key={item.title}>
+        <div className="title active">
+          <i className="dropdown icon">{item.title}</i>
+        </div>
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </div>
+    )
+  })
+
+  return <div className="ui styled accordion">{renderedItems}</div>
+}
+
+export default Accordion
